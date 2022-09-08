3e09c2170a1f236be8b42f6eb1e3302132241dd8
Nora Mbee <Kafonya.mbee@gmail.com>
2022-09-08 21:27:44 +0100

    Add files via upload

---
+++ module1.py
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Kafonya Nora
+#
+# Created:     14/07/2021
+# Copyright:   (c) Kafonya Nora 2021
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
+gender = input("Enter Your Gender")
+gender = gender.lower()
+
+if gender == "female":
+    print("You are a female")
+
+else:
+    print ("You are a male")
---
+++ module4.py
@@ -0,0 +1,42 @@
+#-------------------------------------------------------------------------------
+# Name:        module4
+# Purpose:
+#
+# Author:      Kafonya Nora
+#
+# Created:     14/07/2021
+# Copyright:   (c) Kafonya Nora 2021
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
+score = int(input("Enter your score"))
+
+
+
+if score == "-1" and age <=-1:
+    print("Invalid Score")
+
+elif score == "0" and age <=39:
+    print("F")
+
+elif score == "40" and age <=40:
+    print("E")
+
+elif score == "45" and age <=45:
+    print("D")
+
+elif score == "50" and age <=50:
+    print("C")
+
+elif score == "60" and age <=60:
+    print("B")
+
+elif score == "70" and age <=70:
+    print("A")
+
